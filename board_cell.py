89132b5737da063cee361d780ec85f9d23018cd0
ztan-mines <ztan@mines.edu>
2021-04-12 00:53:36 -0600

    refactored

---
+++ board_cell.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3
+
+import colors
+
+class BoardCell:
+    is_occupied = True
+    color = colors.WHITE
--- main.py
+++ main.py
@@ -1,29 +1,16 @@
 #!/usr/local/bin/python3
 
-import numpy as np
 import pygame
-import random
+import colors
+from tetris import Tetris
 
-TETRIS_VALS = {
-    "board_height": 20,
-    "board_width": 10,
-    "board_ul_x": 20,
-    "board_ul_y": 50,
-    "space_width": 50
-}
-GUI_VALS = {
-    "resolution": (800, 1080)
-}
-COLORS = {
-    "black": (0, 0, 0),
-    "white": (255, 255, 255),
-    "gray": (128, 128, 128)
-}
+res = (800, 1080)
 
 def main():
-    res = (800, 1000)  # TODO: global var
+    res = (800, 1500)  # TODO: global var
+    tetris = Tetris()
     pygame.init()
-    screen = pygame.display.set_mode(GUI_VALS["resolution"])
+    screen = pygame.display.set_mode(res)
     done = False
     while not done:
 
@@ -33,58 +20,41 @@ def main():
                 done = True
         
         # draw background
-        screen.fill(COLORS["white"])
+        screen.fill(colors.WHITE)
 
-        # draw grid
-        for i in range(TETRIS_VALS["board_height"]):
-            for j in range(TETRIS_VALS["board_width"]):
+        # draw board
+        for i in range(tetris.board_height):
+            for j in range(tetris.board_width):
+
+                # draw grid
                 pygame.draw.rect(
                     screen, 
-                    COLORS["gray"], 
+                    colors.GRAY, 
                     [
-                        TETRIS_VALS["board_ul_x"] + TETRIS_VALS["space_width"] * j, 
-                        TETRIS_VALS["board_ul_x"] + TETRIS_VALS["space_width"] * i, 
-                        TETRIS_VALS["space_width"], 
-                        TETRIS_VALS["space_width"]
+                        tetris.grid_x + tetris.square_width * j, 
+                        tetris.grid_y + tetris.square_width * i, 
+                        tetris.square_width, 
+                        tetris.square_width
                     ], 
                     1
                 )
 
-        # draw set tetrominos
-
-        # draw active tetromino
+                # draw tetrominos
+                if tetris.board[i][j].is_occupied:
+                    pygame.draw.rect(
+                        screen,
+                        tetris.board[i][j].color,
+                        [
+                            tetris.grid_x + tetris.square_width * j + 1,
+                            tetris.grid_y + tetris.square_width * i + 1,
+                            tetris.square_width - 2,
+                            tetris.square_width - 1
+                        ]
+                    )
 
         # update drawings
         pygame.display.flip()
-        
-
-    # """
-    # create game board
-    
-
-    # while game not over:
-    #     get new tetromino
-    #     call task planner
-    #     call motion planner
-    #     perform plan
-    #     check game over
-    # """
-    # print('hello world')
-    # res = (800, 1000)
-    # pygame.init()
-    # screen = pygame.display.set_mode(res)
-    # done = False
-    # while not done:
-    #     pygame.display.set_caption("Test")
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.QUIT:
-    #             done = True
-    #     screen.fill(WHITE)
-    #     pygame.draw.rect(screen, GRAY, pygame.Rect(100, 100, 100, 100))
-    #     pygame.display.flip()
-    #     # pygame.draw.rect(screen, WHITE, [1, 2, 3], 1)
-        
-    
+  
 
 if __name__ == "__main__":
     main()
